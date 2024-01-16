fa236d56e485583ce7d0f9092a9adeed02f43e87
TTopopo <134698978+TTopopo@users.noreply.github.com>
2024-01-16 11:17:03 +0300

    Добавления элементом
    Добавления элементом на форму

---
+++ Exzamen/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Exzamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(389, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(658, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сохранить  в DOC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(478, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Отчет о введении режима неполного рабочего дня (смены) и (или) неполной рабочей н" +
+    "едели";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(308, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Дата";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "(дата представления отчета) N";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(448, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(51, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(482, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(51, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "(номер, присвоенный отчету)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(680, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = " 1. Полное наименование отчитывающейся организации/фамилия, имя, отчество (при на" +
+    "личии) индивидуального предпринимателя";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(94, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(51, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(94, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(694, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "2. Отчет представлен (выбрать значение: юридическое лицо; филиал; представительст" +
+    "во; обособленное структурное подразделение) ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(97, 173);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(51, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(94, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(261, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "3. Организационно-правовая форма (по ОКОПФ) ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(361, 196);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(51, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Exzamen/Form1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Office.Interop.Word;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+namespace Exzamen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Создание нового документа Word
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = wordApp.Documents.Add();
+
+            // Заполнение документа данными из текстового поля
+            Word.Paragraph paragraph = doc.Content.Paragraphs.Add();
+
+            paragraph.Range.Text = " " + label1.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = " " + label2.Text;
+            paragraph.Range.InsertParagraphAfter(); 
+            paragraph.Range.Text = "  " + textBox1.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + textBox2.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + textBox3.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + label3.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + textBox4.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + label4.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + label5.Text;
+            paragraph.Range.InsertParagraphAfter(); 
+            paragraph.Range.Text = "  " + textBox4.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + label6.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + textBox5.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + label7.Text;
+            paragraph.Range.InsertParagraphAfter();
+            paragraph.Range.Text = "  " + textBox7.Text;
+            paragraph.Range.InsertParagraphAfter();
+            // Сохранение документа
+            object fileName = "5.doc";
+            doc.SaveAs2(ref fileName);
+            doc.Close();
+            wordApp.Quit();
+
+            MessageBox.Show("Документ успешно сохранен.");
+        }
+    }
+}
